33ddf09380de79516d7a5f7265a5dca604035753
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-02-28 11:11:44 +0100

    DB and first routes creation

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,6 @@
 export const PORT = 5555;
+
+export const mongoDRURL = 'mongodb+srv://gwartelle:tU8X1HRF6BCFGqSY@bookstore.f8s5hi7.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore'
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial.
